697d3cc87c39e5d77c83d49d2046fa392c0600cd
lead-1 <lead-1@octeams.local>
2026-02-19 20:02:59 +0000

    Bump to 1.0.4 - clickable URL

--- Formula/mixaroo-lite.rb
+++ Formula/mixaroo-lite.rb
@@ -1,8 +1,8 @@
 class MixarooLite < Formula
   desc "AI-powered CLI tool that generates playlists and creates YouTube playback links"
   homepage "https://github.com/schoolofai/mixaroo-lite"
-  url "https://registry.npmjs.org/mixaroo-lite/-/mixaroo-lite-1.0.3.tgz"
-  sha256 "c164d3bf272d3437d8139bf0ec3a4081efcf964946259e35760cc1126d0d8f08"
+  url "https://registry.npmjs.org/mixaroo-lite/-/mixaroo-lite-1.0.4.tgz"
+  sha256 "479732d45f91222e7d46d0a29a5ef6e0140254ac77e2d55a1abec63eac023e48"
   license "MIT"
 
   depends_on "node"
